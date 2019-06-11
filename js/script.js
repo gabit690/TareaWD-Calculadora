f0bb8b83716587382031800dbf802f7f062dbee3
Gabit <gabit690@gmail.com>
2019-06-10 21:59:37 -0300

    Estructura general de los elementos de la calculadora sin funcionalidades

---
+++ js/script.js
@@ -0,0 +1,10 @@
+'use strict'
+
+/*
+5. Realizar una calculadora básica teniendo en cuanta los operadores básicos (suma, resta, división y multiplicación).
+Deberán utilizar HTML, CSS y JavaScript para la misma.
+
+Ejemplo:
+    Tener en cuenta que sucede si el usuario ingresa un 0 cuando quiere generar la división. Se deberá dar una respuesta en cada caso.
+*/
+
